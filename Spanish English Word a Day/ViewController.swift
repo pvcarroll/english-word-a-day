60f02d1bdaabc30a647d7e804d72746178bbdbe3
Paul Carroll <p.v.carroll@gmail.com>
2018-12-14 23:39:58 -0600

    Initial Commit

---
+++ Spanish English Word a Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spanish English Word a Day
+//
+//  Created by Paul Carroll on 12/14/18.
+//  Copyright © 2018 Paul Carroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
