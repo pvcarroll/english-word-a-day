56fe6670e0ac0cd002b84cb01f341cca10c77d98
Paul Carroll <p.v.carroll@gmail.com>
2018-12-24 17:18:59 -0600

    Update message when lang switched

--- Spanish English Word a Day/ViewController.swift
+++ Spanish English Word a Day/ViewController.swift
@@ -11,17 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var languageControl: UISegmentedControl!
+    @IBOutlet weak var mainMessageLabel: UILabel!
     @IBOutlet weak var timePicker: UIDatePicker!
     @IBOutlet weak var setupNotificationsButton: UIButton!
+    
+    var selectedLanguageIndex = 0
+    
+    @IBAction func switchLanguage(_ sender: UISegmentedControl) {
+        selectedLanguageIndex = sender.selectedSegmentIndex
+        updateStrings(selectedSegmentIndex: sender.selectedSegmentIndex)
+    }
     
     @IBAction func setupNotifications(_ sender: UIButton) {
         print("date = \(self.timePicker.date)")
         Notifications().setup(notificationTime: self.timePicker.date, completion: {
-            let alertController = UIAlertController(
-                title: "Notificaciones Programadas (Notifications Scheduled)",
-                message: "Recibirá 1 notificación al día durante los próximos 63 días.  (You will receive 1 notification a day for the next 63 days)",
-                preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Aceptar (OK)", style: .default, handler: nil)
+            let title: String
+            let message: String
+            let okMessage: String
+            if self.selectedLanguageIndex == 0 {
+                title = Messages.NotificationsScheduledTitle.spanish.rawValue
+                message = Messages.NotificationsScheduledBody.spanish.rawValue
+                okMessage = Messages.Ok.spanish.rawValue
+            } else {
+                title = Messages.NotificationsScheduledTitle.english.rawValue
+                message = Messages.NotificationsScheduledBody.english.rawValue
+                okMessage = Messages.Ok.english.rawValue
+            }
+            let alertController = UIAlertController(title: title,
+                                                    message: message,
+                                                    preferredStyle: .alert)
+            let okAction = UIAlertAction(title: okMessage, style: .default, handler: nil)
             alertController.addAction(okAction)
             DispatchQueue.main.async {
                 self.present(alertController, animated: true)
@@ -32,8 +51,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Learn a Word a Day"
+        mainMessageLabel.text = Messages.MainMessage.spanish.rawValue
         self.setupNotificationsButton.layer.cornerRadius = 10
         self.setupNotificationsButton.layer.borderWidth = 1
         self.setupNotificationsButton.layer.borderColor = UIColor(red:0.20, green:0.29, blue:0.81, alpha:1.0).cgColor
+    }
+    
+    private func updateStrings(selectedSegmentIndex: Int) {
+        if selectedSegmentIndex == 0 {
+            mainMessageLabel.text = Messages.MainMessage.spanish.rawValue
+            setupNotificationsButton.setTitle(Messages.SetupNotifications.spanish.rawValue, for: .normal)
+        } else {
+            mainMessageLabel.text = Messages.MainMessage.english.rawValue
+            setupNotificationsButton.setTitle(Messages.SetupNotifications.english.rawValue, for: .normal)
+        }
+    }
+}
+
+struct Messages {
+    enum MainMessage: String {
+        case english = "Pick a time and receive daily notifications with an English word"
+        case spanish = "Elija una hora y reciba notificaciones diarias con una palabra en inglés"
+    }
+    
+    enum SetupNotifications: String {
+        case english = "Setup Notifications"
+        case spanish = "Configurar Notificaciones"
+    }
+    
+    enum NotificationsScheduledTitle: String {
+        case english = "Notifications Scheduled"
+        case spanish = "Notificaciones Programadas"
+    }
+    
+    enum NotificationsScheduledBody: String {
+        case english = "You will receive 1 notification a day for the next 63 days."
+        case spanish = "Recibirá 1 notificación al día durante los próximos 63 días."
+    }
+    
+    enum Ok: String {
+        case english = "OK"
+        case spanish = "Aceptar"
     }
 }
